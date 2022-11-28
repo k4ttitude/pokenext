6c170b3c4a47789f5074b7a19941b89a1f8b393c
Pham Minh Hieu <hieupm@vnext.com.vn>
2022-11-28 16:25:53 +0700

    wip: root layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,27 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+    <main className="h-screen flex flex-col items-center">
+      <div className="bg-red-50 w-min px-3 py-2 flex flex-col">
+        <h2>Pokemon Search</h2>
+        <div>
+          Name <input />
         </div>
-      </main>
+        <div>
+          Type{" "}
+          <select>
+            <option>Electric</option>
+          </select>
+        </div>
+        <div>
+          <input type="checkbox" />
+          <label>is legendary</label>
+          <input type="checkbox" />
+          <label>is mythical</label>
+        </div>
+      </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
+      <div>Showing 1-3 results from 54</div>
+      <div className="flex-1 flex flex-col items-stretch"></div>
+    </main>
+  );
 }
