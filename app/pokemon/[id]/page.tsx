7dc05c4cc0d26b8a8feb9578fd6f2755847f58b7
Pham Minh Hieu <hieupm@vnext.com.vn>
2022-12-01 04:47:31 +0700

    abilities

--- app/pokemon/[id]/page.tsx
+++ app/pokemon/[id]/page.tsx
@@ -20,72 +20,85 @@ export default async function Page({ params }: Props) {
     );
 
   return (
-    <div className="flex flex-col gap-1 bg-yellow-300">
-      <h2 className="font-bold text-2xl uppercase mb-4 self-center">
+    <div className="flex flex-col bg-yellow-300 items-center">
+      <h2 className="font-bold text-2xl uppercase mb-4">
         Pokemon search results
       </h2>
-      <div className="flex flex-col gap-1 p-2 rounded-lg bg-white/50 self-center">
-        <div className="flex gap-1">
-          <Content className="flex-1 flex justify-evenly">
-            <div className="flex-[2] flex flex-col items-center">
-              <span className="font-bold text-lg">{englishName?.name}</span>
-              <span className="font-bold">
-                {
-                  pokemon.pokemon_v2_pokemonspecy.pokemon_v2_pokemonspeciesnames.find(
-                    (item) => item.pokemon_v2_language.name === "ja"
-                  )?.name
-                }
-              </span>
-            </div>
-            <div className="flex-1 flex items-center justify-center text-center text-sm">
-              {englishName?.genus}
-            </div>
+      <div className="max-w-lg w-full flex flex-col gap-1">
+        <Section>
+          <div className="flex gap-1">
+            <Content className="flex-1 flex justify-evenly">
+              <div className="flex-[2] flex flex-col items-center">
+                <span className="font-bold text-lg">{englishName?.name}</span>
+                <span className="font-bold">
+                  {
+                    pokemon.pokemon_v2_pokemonspecy.pokemon_v2_pokemonspeciesnames.find(
+                      (item) => item.pokemon_v2_language.name === "ja"
+                    )?.name
+                  }
+                </span>
+              </div>
+              <div className="flex-1 flex items-center justify-center text-center text-sm">
+                {englishName?.genus}
+              </div>
+            </Content>
+            <Content className="aspect-square flex items-center justify-center font-bold text-lg">
+              #{formatId(pokemon.id, 3)}
+            </Content>
+          </div>
+          <Content>
+            <Image
+              src="/sprites/sprites/pokemon/1.png"
+              alt="Pikachu"
+              className="w-full"
+              width={200}
+              height={200}
+            />
           </Content>
-          <Content className="aspect-square flex items-center justify-center font-bold text-lg">
-            #{formatId(pokemon.id, 3)}
+        </Section>
+        <Section>
+          <Title>Types</Title>
+          <Content className="flex gap-1 flex-wrap justify-center">
+            {pokemon.pokemon_v2_pokemontypes
+              .map((item) => item.pokemon_v2_type.name)
+              .map((type, idx) => (
+                <span
+                  key={`type_${idx}`}
+                  className={`rounded px-2 py-1 capitalize leading-none bg-pokemon-${type}`}
+                >
+                  {type}
+                </span>
+              ))}
           </Content>
-        </div>
-        <Content>
-          <Image
-            src="/sprites/sprites/pokemon/1.png"
-            alt="Pikachu"
-            className="w-full"
-            width={200}
-            height={200}
-          />
-        </Content>
+        </Section>
+        <Section>
+          <Title>Abilities</Title>
+          <Content className="grid grid-cols-2">
+            {pokemon.pokemon_v2_pokemonabilities
+              .map(
+                (item) =>
+                  item.pokemon_v2_ability.pokemon_v2_abilitynames[0].name
+              )
+              .map((ability) => (
+                <span key={`ability_${ability}`} className="text-center">
+                  {ability}
+                </span>
+              ))}
+          </Content>
+        </Section>
+        <Section>
+          <Title>Height</Title>
+        </Section>
+        <Section>
+          <Title>Weight</Title>
+        </Section>
       </div>
-      <Section>
-        <Title>Types</Title>
-        <Content>
-          {pokemon.pokemon_v2_pokemontypes
-            .map((item) => item.pokemon_v2_type.name)
-            .map((type, idx) => (
-              <span
-                key={`type_${idx}`}
-                className={`rounded px-2 py-1 capitalize leading-none bg-pokemon-${type}`}
-              >
-                {type}
-              </span>
-            ))}
-        </Content>
-      </Section>
-      <Section>
-        <Title>Abilities</Title>
-        <Content></Content>
-      </Section>
-      <Section>
-        <Title>Height</Title>
-      </Section>
-      <Section>
-        <Title>Weight</Title>
-      </Section>
     </div>
   );
 }
 
 const Section = ({ children }: PropsWithChildren) => (
-  <div className="flex flex-col gap-1 p-2 rounded-lg bg-white/50 self-center">
+  <div className="w-full flex flex-col gap-1 p-2 rounded-lg bg-white/50 self-center">
     {children}
   </div>
 );
