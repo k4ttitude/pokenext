83359b240078857777f3d24b3eac476484fa3610
Pham Minh Hieu <hieupm@vnext.com.vn>
2022-12-01 07:14:26 +0700

    hasura header

--- graphql/client.ts
+++ graphql/client.ts
@@ -1,11 +1,12 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
-console.log(process.env.NEXT_PUBLIC_GRAPHQL_URI, process.env.GRAPHQL_URI);
-
 export const ssrClient = new ApolloClient({
   ssrMode: true,
   link: new HttpLink({
     uri: process.env.GRAPHQL_URI,
+    headers: {
+      "x-hasura-admin-secret": "pokemon",
+    },
   }),
   cache: new InMemoryCache(),
 });
@@ -13,4 +14,7 @@ export const ssrClient = new ApolloClient({
 export const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
   cache: new InMemoryCache(),
+  headers: {
+    "x-hasura-admin-secret": "pokemon",
+  },
 });
